56c8762ce38cd0a7e182b43ec11d9bd0619ef7a8
NataliaRojas <np.rojas1158@uniandes.edu.co>
2024-11-29 16:57:17 -0500

    se agrega import new relis

--- blacklists/src/application.py
+++ blacklists/src/application.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv, find_dotenv
 import os
 loaded = load_dotenv(os.path.join(os.path.dirname(__file__), '..','.env.template'))
 
+import newrelic.agent
+newrelic.agent.initialize()
+
 from .errors.errors import ApiError
 from .blueprints.blacklists import blacklists_blueprint
 from .model.emailBlacklist import EmailBlacklist
